d99ca3699d137428ed63ee3030f205a9049b950c
samer <rouphaels@gmail.com>
2023-07-25 15:13:58 +0300

    done with task_2

---
+++ Task_2/Task_2.js
@@ -0,0 +1,21 @@
+var a =2;
+var b =4;
+var c = a+b;
+
+let d = 2;
+let e =5;
+let f = d+e;
+
+const x =2;
+const y = 5 ;
+const z = x+y;
+ 
+
+//string
+//number
+//boolean
+//object
+//function
+
+var user ="samer"
+console.log(user);
