caa31887cf20d42df2a30af6e3de92a5bfacf3c7
samer <rouphaels@gmail.com>
2023-07-25 16:04:10 +0300

    done with task_3

---
+++ Task_3/Task_3.js
@@ -0,0 +1,13 @@
+const num1 = "samer";
+var num2 = "rouphael";
+var canpass = true;
+if (canpass) 
+    if (num1 >= num2) {
+        console.log("hello");
+    }
+    else { console.log("bye"); }
+
+
+else {
+        console.log("Try again");
+    }
