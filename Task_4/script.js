8658747eae9b7b9b2a2db0a9e175bedd94e0a18e
samer <rouphaels@gmail.com>
2023-07-26 10:23:28 +0300

    Task_4

---
+++ Task_4/script.js
@@ -0,0 +1,12 @@
+let values= [1,2,3];
+console.log(values0)
+console.log(values1)
+console.log(values2)
+
+
+let valuess=newArray(samer,hanna,rouphael)
+console.log(valuess0)
+console.log(vlauess1)
+console.log(valuess2)
+
+Array1=
