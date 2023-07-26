e18e5cbcdfa513848cc9a20d026cb3cdb2bdc7c3
samer <rouphaels@gmail.com>
2023-07-26 13:00:22 +0300

    Task_5

--- Task_5/script.js
+++ Task_5/script.js
@@ -1,14 +1,15 @@
-Array= [1,2, 3, 4, 5, 6, 7, 8]
+Array = [1, 2, 3, 4, 5, 6, 7, 8]
 
 i = 0;
 console.log(Array.length)
-for (i = 0; i < Array.length; i++){
+for (i = 0; i < Array.length; i++) {
 
 
-let remainder = (Array[i] % 2);
-if (remainder == 0) {
-    console.log('even number'+Array[i])
+    let remainder = (Array[i] % 2);
+    if (remainder == 0) {
+        console.log('even number' + Array[i])
+    }
+    else {
+        console.log('odd number' + Array[i])
+    }
 }
-else {
-    console.log('odd number'+Array[i])
-}}
