d8690ea39bdc3138b5a33f53aaa6d7021f083d65
samer <rouphaels@gmail.com>
2023-07-27 14:20:31 +0300

    Task_9

---
+++ Task_9/script.js
@@ -0,0 +1,39 @@
+
+var p1;
+var p2;
+function myFunctionadd(p1, p2) {
+
+    console.log(p1 + p2)
+}
+
+function myFunctionsubstract(p1, p2) {
+    console.log(p1 - p2)
+}
+
+function myFunctionmultiply(p1, p2) {
+    console.log(p1 * p2);
+}
+
+function myFunctiondivide(p1, p2) {
+    console.log(p1 / p2)
+}
+
+myFunctionadd(4, 2);
+myFunctiondivide(4, 2);
+myFunctionmultiply(4, 2);
+myFunctionsubstract(4, 2);
+
+FunctionCalculator(type, num1, num2)
+if (type = add) {
+    console.log(num1 + num2)
+    elseif(type = substract)
+    console.log(num1 - num2)
+
+    elseif(type = mul);
+    { console.log(num1 * num2) }
+}
+else (type = divide)
+console.log(num1 / num2)
+
+
+
